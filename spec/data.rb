8036ad2f3198c80f18ea5a0023bdf169ed54072f
ianaré <97107275+ianardee@users.noreply.github.com>
2023-04-24 18:29:16 +0200

    :art: split each document type into its own spec file (#21)

--- spec/data.rb
+++ spec/data.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 DATA_DIR = File.join(__dir__, 'data').freeze
-DIR_CUSTOM_V1 = File.join(DATA_DIR, 'custom', 'response_v1').freeze
-DIR_INVOICE_V4 = File.join(DATA_DIR, 'invoice', 'response_v4').freeze
-DIR_RECEIPT_V4 = File.join(DATA_DIR, 'receipt', 'response_v4').freeze
-DIR_FINANCIAL_DOCUMENT_V1 = File.join(DATA_DIR, 'financial_document', 'response_v1').freeze
-DIR_PROOF_OF_ADDRESS_V1 = File.join(DATA_DIR, 'proof_of_address', 'response_v1').freeze
-DIR_PASSPORT_V1 = File.join(DATA_DIR, 'passport', 'response_v1').freeze
-DIR_EU_LICENSE_PLATE_V1 = File.join(DATA_DIR, 'eu', 'license_plate', 'response_v1').freeze
-DIR_SHIPPING_CONTAINER_V1 = File.join(DATA_DIR, 'shipping_container', 'response_v1').freeze
-DIR_US_BANK_CHECK_V1 = File.join(DATA_DIR, 'us', 'bank_check', 'response_v1').freeze
-DIR_FR_BANK_ACCOUNT_DETAILS_V1 = File.join(DATA_DIR, 'fr', 'bank_account_details', 'response_v1').freeze
-DIR_FR_CARTE_VITALE_V1 = File.join(DATA_DIR, 'fr', 'carte_vitale', 'response_v1').freeze
-DIR_FR_ID_CARD_V1 = File.join(DATA_DIR, 'fr', 'id_card', 'response_v1').freeze
+
+def load_json(dir_path, file_name)
+  file_data = File.read(File.join(dir_path, file_name))
+  JSON.parse(file_data)
+end
+
+def read_file(dir_path, file_name)
+  File.read(File.join(dir_path, file_name)).strip
+end
---
+++ spec/document/us/bank_check_v1_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'mindee/parsing'
+
+require_relative '../../data'
+
+DIR_US_BANK_CHECK_V1 = File.join(DATA_DIR, 'us', 'bank_check', 'response_v1').freeze
+
+describe Mindee::Prediction::US::BankCheckV1 do
+  context 'A US Bank Check V1' do
+    it 'should load an empty document prediction' do
+      response = load_json(DIR_US_BANK_CHECK_V1, 'empty.json')
+      inference = Mindee::Document.new(Mindee::Prediction::US::BankCheckV1, response['document']).inference
+      expect(inference.product.type).to eq('standard')
+      expect(inference.prediction.account_number.value).to be_nil
+    end
+
+    it 'should load a complete document prediction' do
+      to_string = read_file(DIR_US_BANK_CHECK_V1, 'summary_full.rst')
+      response = load_json(DIR_US_BANK_CHECK_V1, 'complete.json')
+      document = Mindee::Document.new(Mindee::Prediction::US::BankCheckV1, response['document'])
+      inference = document.inference
+      expect(inference.prediction.account_number.value).to eq('12345678910')
+      expect(inference.prediction.check_position.rectangle.top_left.y).to eq(0.129)
+      expect(inference.prediction.check_position.rectangle[0][1]).to eq(0.129)
+      inference.prediction.signatures_positions.each do |pos|
+        expect(pos).to be_a_kind_of(Mindee::PositionField)
+      end
+      expect(document.to_s).to eq(to_string)
+    end
+
+    it 'should load a complete page 0 prediction' do
+      to_string = read_file(DIR_US_BANK_CHECK_V1, 'summary_page0.rst')
+      response = load_json(DIR_US_BANK_CHECK_V1, 'complete.json')
+      document = Mindee::Document.new(Mindee::Prediction::US::BankCheckV1, response['document'])
+      page = document.inference.pages[0]
+      expect(page.prediction.account_number.value).to eq('12345678910')
+      expect(page.prediction.check_position.rectangle.top_left.y).to eq(0.129)
+      expect(page.prediction.check_position.rectangle[0][1]).to eq(0.129)
+      page.prediction.signatures_positions.each do |pos|
+        expect(pos).to be_a_kind_of(Mindee::PositionField)
+      end
+      expect(page.to_s).to eq(to_string)
+    end
+  end
+end
