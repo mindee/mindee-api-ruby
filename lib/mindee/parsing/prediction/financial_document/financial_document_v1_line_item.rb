917b78900b387609f7a35bee5ceec9ef93b3d7f8
Sebastian Olivera <sebastian.oliverasilvera@mindee.co>
2023-06-14 14:11:42 +0200

    functionnal financial doc

--- lib/mindee/parsing/prediction/financial_document/financial_document_v1_line_item.rb
+++ lib/mindee/parsing/prediction/financial_document/financial_document_v1_line_item.rb
@@ -1,24 +1,29 @@
 # frozen_string_literal: true
 
-require_relative '../common_fields/base'
-
 module Mindee
-  # Line items for invoices
+  # List of line item details.
   class FinancialDocumentV1LineItem
-    # @return [String] The product code referring to the item.
-    attr_reader :product_code
+    # The item description.
     # @return [String]
     attr_reader :description
+    # The product code referring to the item.
+    # @return [String]
+    attr_reader :product_code
+    # The item quantity
     # @return [Float]
     attr_reader :quantity
-    # @return [Float]
-    attr_reader :unit_price
-    # @return [Float]
-    attr_reader :total_amount
-    # @return [Float] The item tax rate percentage.
-    attr_reader :tax_rate
+    # The item tax amount.
     # @return [Float]
     attr_reader :tax_amount
+    # The item tax rate in percentage.
+    # @return [Float]
+    attr_reader :tax_rate
+    # The item total amount.
+    # @return [Float]
+    attr_reader :total_amount
+    # The item unit price.
+    # @return [Float]
+    attr_reader :unit_price
     # @return [Float]
     attr_reader :confidence
     # @return [Integer]
@@ -29,20 +34,31 @@ module Mindee
     attr_reader :polygon
 
     def initialize(prediction, page_id)
+      @description = prediction['description']
       @product_code = prediction['product_code']
       @quantity = prediction['quantity']
-      @unit_price = prediction['unit_price']
-      @total_amount = prediction['total_amount']
       @tax_amount = prediction['tax_amount']
       @tax_rate = prediction['tax_rate']
-      @description = prediction['description']
+      @total_amount = prediction['total_amount']
+      @unit_price = prediction['unit_price']
       @page_id = page_id
     end
 
+    # Format strings for display by shortening long strings and assigning empty ones.
+    # @param input_str [String, nil]
+    # @param max_size [int]
+    # @return [String]
+    def format_for_display(in_str, max_size)
+      return '' if in_str.nil?
+
+      in_str.length < max_size ? in_str : "#{in_str[0..max_size - 3]}..."
+    end
+
+    # @return [Hash]
     def printable_values
       printable = {}
-      printable[:description] = @description.nil? ? '' : @description
-      printable[:product_code] = @product_code.nil? ? '' : @product_code
+      printable[:description] = format_for_display(@description, 36)
+      printable[:product_code] = format_for_display(@product_code, 12)
       printable[:quantity] = @quantity.nil? ? '' : Field.float_to_string(@quantity)
       printable[:tax_amount] = @tax_amount.nil? ? '' : Field.float_to_string(@tax_amount)
       printable[:tax_rate] = @tax_rate.nil? ? '' : Field.float_to_string(@tax_rate)
@@ -51,32 +67,32 @@ module Mindee
       printable
     end
 
-    # @return String
-    def to_s
-      printable = printable_values
-      out_str = String.new
-      out_str << ("Description: #{printable[:description][0..33]}" + (printable[:description].length <= 33 ? '' : '...'))
-      out_str << "Product code: #{printable[:product_code]}"
-      out_str << "Quantity: #{printable[:quantity]}"
-      out_str << "Tax Amount: #{printable[:tax_amount]}"
-      out_str << "Tax Rate (%): #{printable[:tax_rate]}"
-      out_str << "Total Amount: #{printable[:total_amount]}"
-      out_str << "Unit Price: #{printable[:unit_price]}"
-      out_str.strip
-    end
-
-    # @return String
+    # @return [String]
     def to_table_line
       printable = printable_values
       out_str = String.new
-      out_str << ("| #{printable[:description].ljust(36, ' ')}")
-      out_str << (" | #{printable[:product_code].ljust(12, ' ')}")
-      out_str << (" | #{printable[:quantity].ljust(8, ' ')}")
-      out_str << (" | #{printable[:tax_amount].ljust(10, ' ')}")
-      out_str << (" | #{printable[:tax_rate].ljust(12, ' ')}")
-      out_str << (" | #{printable[:total_amount].ljust(12, ' ')}")
-      out_str << (" | #{printable[:unit_price].ljust(10, ' ')} |")
-      out_str.rstrip
+      out_str << format('| %- 37s', printable[:description])
+      out_str << format('| %- 13s', printable[:product_code])
+      out_str << format('| %- 9s', printable[:quantity])
+      out_str << format('| %- 11s', printable[:tax_amount])
+      out_str << format('| %- 13s', printable[:tax_rate])
+      out_str << format('| %- 13s', printable[:total_amount])
+      out_str << format('| %- 11s', printable[:unit_price])
+      out_str << '|'
+    end
+
+    # @return [String]
+    def to_s
+      printable = printable_values
+      out_str = String.new
+      out_str << printable[:description]
+      out_str << printable[:product_code]
+      out_str << printable[:quantity]
+      out_str << printable[:tax_amount]
+      out_str << printable[:tax_rate]
+      out_str << printable[:total_amount]
+      out_str << printable[:unit_price]
+      out_str.strip
     end
   end
 end
