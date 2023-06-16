70ed4925a6dbb9fa2f109c64fb4b6c61fd17869b
Sebastian <130448732+sebastianMindee@users.noreply.github.com>
2023-06-16 15:16:22 +0200

    :sparkles: add url input source (#35)

--- lib/mindee/input/sources.rb
+++ lib/mindee/input/sources.rb
@@ -93,5 +93,17 @@ module Mindee
         super(io_stream, filename)
       end
     end
+
+    # Load a remote document from a file url.
+    class UrlInputSource
+      # @return [String]
+      attr_reader :url
+
+      def initialize(url)
+        raise 'URL must be HTTPS' unless url.start_with? 'https://'
+
+        @url = url
+      end
+    end
   end
 end
