6fb30bcd29db076d073fc746385759db7b1406ef
Sebastian Olivera <sebastian.oliverasilvera@mindee.co>
2023-06-19 09:46:18 +0200

    removed unnecessary comments

--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -18,12 +18,3 @@ describe Mindee::Ocr::Ocr do
     end
   end
 end
-
-# def test_response
-#   json_data = json.load(open("./data/ocr/complete_with_ocr.json"))
-#   with open("./tests/data/ocr/ocr.txt") as file_handle:
-#       expected_text = file_handle.read()
-#   ocr = Ocr(json_data["document"]["ocr"])
-#   assert str(ocr) == expected_text
-#   assert str(ocr.mvision_v1.pages[0]) == expected_text
-# end
