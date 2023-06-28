c37da848c6bfbbe737a357702335118b8632aa54
Sebastian <130448732+sebastianMindee@users.noreply.github.com>
2023-06-28 18:05:06 +0200

    :art: move loose modules to a better spot (#41)

--- spec/async_rseponse_spec.rb
+++ spec/async_rseponse_spec.rb
@@ -11,7 +11,7 @@ describe Mindee::Parsing::Common::ApiResponse do
       response = load_json(DIR_ASYNC, 'post_success.json')
       parsed_response = Mindee::Parsing::Common::ApiResponse.new(Mindee::Product::InvoiceSplitter::InvoiceSplitterV1,
                                                                  response)
-      expect(parsed_response.job.status).to eq(Mindee::JobStatus::WAITING)
+      expect(parsed_response.job.status).to eq(Mindee::Parsing::Common::JobStatus::WAITING)
       expect(parsed_response.job.id).to eq('76c90710-3a1b-4b91-8a39-31a6543e347c')
       expect(parsed_response.job.status).to_not respond_to(:available_at)
       expect(parsed_response.job.status).to_not respond_to(:millisecs_taken)
@@ -26,7 +26,7 @@ describe Mindee::Parsing::Common::ApiResponse do
       expect(parsed_response.job.id).to be(nil)
       expect(parsed_response.job.status).to_not respond_to(:available_at)
       expect(parsed_response.job.status).to_not respond_to(:millisecs_taken)
-      expect(parsed_response.api_request.status).to eq(Mindee::RequestStatus::FAILURE)
+      expect(parsed_response.api_request.status).to eq(Mindee::Parsing::Common::RequestStatus::FAILURE)
       expect(parsed_response.api_request.error['code']).to eq('Forbidden')
     end
 
@@ -35,7 +35,7 @@ describe Mindee::Parsing::Common::ApiResponse do
       parsed_response = Mindee::Parsing::Common::ApiResponse.new(Mindee::Product::InvoiceSplitter::InvoiceSplitterV1,
                                                                  response)
       expect(parsed_response.job.issued_at.strftime('%Y-%m-%dT%H:%M:%S.%6N')).to eq('2023-03-16T12:33:49.602947')
-      expect(parsed_response.job.status).to eq(Mindee::JobStatus::PROCESSING)
+      expect(parsed_response.job.status).to eq(Mindee::Parsing::Common::JobStatus::PROCESSING)
       expect(parsed_response.job.id).to eq('76c90710-3a1b-4b91-8a39-31a6543e347c')
       expect(parsed_response.job.status).to_not respond_to(:available_at)
       expect(parsed_response.job.status).to_not respond_to(:millisecs_taken)
@@ -47,7 +47,7 @@ describe Mindee::Parsing::Common::ApiResponse do
       parsed_response = Mindee::Parsing::Common::ApiResponse.new(Mindee::Product::InvoiceSplitter::InvoiceSplitterV1,
                                                                  response)
       expect(parsed_response.job.issued_at.strftime('%Y-%m-%dT%H:%M:%S.%6N')).to eq('2023-03-21T13:52:56.326107')
-      expect(parsed_response.job.status).to eq(Mindee::JobStatus::COMPLETED)
+      expect(parsed_response.job.status).to eq(Mindee::Parsing::Common::JobStatus::COMPLETED)
       expect(parsed_response.job.id).to eq('b6caf9e8-9bcc-4412-bcb7-f5b416678f0d')
       expect(parsed_response.job.available_at.strftime('%Y-%m-%dT%H:%M:%S.%6N')).to eq('2023-03-21T13:53:00.990339')
       expect(parsed_response.job.millisecs_taken).to eq(4664)
