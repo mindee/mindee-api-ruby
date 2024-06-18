309071b62b066662222c305ce78b17d58712e938
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-06-18 15:54:59 +0200

    remove unneeded comments

--- spec/image_extraction/image_extractor_spec.rb
+++ spec/image_extraction/image_extractor_spec.rb
@@ -17,13 +17,8 @@ describe Mindee::ImageExtraction do
 
   context 'an image extractor' do
     it 'extracts barcode images correctly' do
-      # Assuming barcode_path and barcode_json_path are fixtures provided by RSpec
-
-      # Load JSON fixture
       json_data = JSON.parse(File.read(barcode_json_path))
       inference = Mindee::Product::BarcodeReader::BarcodeReaderV1.new(json_data['document']['inference'])
-
-      # Extract polygon coordinates
       barcodes1 = []
       inference.prediction.codes_1d.each do |barcode|
         barcodes1.push(barcode.polygon)
@@ -32,16 +27,11 @@ describe Mindee::ImageExtraction do
       inference.prediction.codes_2d.each do |barcode|
         barcodes2.push(barcode.polygon)
       end
-
-      # Create input source
       input_source = Mindee::Input::Source::PathInputSource.new(barcode_path)
-
-      # Extract images
 
       extracted_barcodes_1d = extract_multiple_images_from_source(input_source, 1, barcodes1)
       extracted_barcodes_2d = extract_multiple_images_from_source(input_source, 1, barcodes2)
 
-      # Assertions
       expect(extracted_barcodes_1d.size).to eq(1)
       expect(extracted_barcodes_2d.size).to eq(2)
 
