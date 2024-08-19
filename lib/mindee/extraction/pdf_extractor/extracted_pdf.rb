1a47a8736bed593da46fe79fe1a1576908fb95bd
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-08-19 12:14:48 +0200

    add pdf extractor

---
+++ lib/mindee/extraction/pdf_extractor/extracted_pdf.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Mindee
+  # Pdf Extraction Module.
+  module PdfExtractor
+    # An extracted sub-Pdf.
+    class ExtractPdf
+      # Byte contents of the pdf
+      # @return [StreamIO]
+      attr_reader :pdf_bytes
+
+      # Name of the file.
+      # @return [String]
+      attr_reader :filename
+
+      # @param pdf_bytes [StreamIO]
+      # @param filename [String]
+      def initialize(pdf_bytes, filename)
+        @pdf_bytes = pdf_bytes
+        @filename = filename
+      end
+
+      # Retrieves the page count for a given pdf.
+      # @return [Integer]
+      def page_count
+        current_pdf = Mindee::PDF::PdfProcessor.open_pdf(pdf_bytes)
+        current_pdf.pages.size
+      rescue TypeError
+        raise 'Could not retrieve page count from Extracted PDF object.'
+      end
+
+      # Writes the contents of the current PDF object to a file.
+      # @param output_path [String] Path to write to.
+      def write_to_file(output_path)
+        raise 'Provided path is not a file' if File.directory?(destination)
+        raise 'Invalid save path provided' unless File.exist?(File.expand_path('..', output_path))
+
+        if File.extname(output_path).downcase == '.pdf'
+          base_path = File.expand_path('..', output_path)
+          output_path = File.expand_path("#{File.basename(output_path)}.pdf", base_path)
+        end
+
+        File.write(output_path, @pdf_bytes)
+      end
+
+      # Returns the current PDF object as a usable BytesInputSource.
+      # @return [Mindee::Input::Source::BytesInputSource]
+      def as_input_source
+        Mindee::Input::Source::BytesInputSource(@pdf_bytes, @filename)
+      end
+    end
+  end
+end
