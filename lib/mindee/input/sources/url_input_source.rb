5ec60cdefc85bdedee850d440b4224da01d19bab
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-12-10 14:22:46 +0100

    :sparkles: add support for remote resource fetching

--- lib/mindee/input/sources/url_input_source.rb
+++ lib/mindee/input/sources/url_input_source.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'uri'
+require 'fileutils'
+
 module Mindee
   module Input
     module Source
@@ -12,6 +16,110 @@ module Mindee
           raise 'URL must be HTTPS' unless url.start_with? 'https://'
 
           @url = url
+        end
+
+        # Downloads the file from the URL and saves it to the specified path.
+        #
+        # @param path [String] Path to save the file to.
+        # @param filename [String, nil] Optional name to give to the file.
+        # @param username [String, nil] Optional username for authentication.
+        # @param password [String, nil] Optional password for authentication.
+        # @param token [String, nil] Optional token for JWT-based authentication.
+        # @param max_redirects [Integer] Maximum amount of redirects to follow.
+        # @return [String] The full path of the saved file.
+        def save_to_file(path, filename: nil, username: nil, password: nil, token: nil, max_redirects: 3)
+          response_body = fetch_file_content(username: username, password: password, token: token,
+                                             max_redirects: max_redirects)
+
+          filename = fill_filename(filename)
+
+          full_path = File.join(path.chomp('/'), filename)
+          File.write(full_path, response_body)
+
+          full_path
+        end
+
+        # Downloads the file from the url, and returns a BytesInputSource wrapper object for it.
+        #
+        # @param filename [String, nil] Optional name to give to the file.
+        # @param username [String, nil] Optional username for authentication.
+        # @param password [String, nil] Optional password for authentication.
+        # @param token [String, nil] Optional token for JWT-based authentication.
+        # @param max_redirects [Integer] Maximum amount of redirects to follow.
+        # @return [BytesInputSource] The full path of the saved file.
+        def as_local_input_source(_path, filename: nil, username: nil, password: nil, token: nil, max_redirects: 3)
+          filename = fill_filename(filename)
+          response_body = fetch_file_content(username: username, password: password, token: token,
+                                             max_redirects: max_redirects)
+          bytes = StringIO.new(response_body)
+
+          BytesInputSource.new(bytes.read, filename)
+        end
+
+        # Fetches the file content from the URL.
+        #
+        # @param username [String, nil] Optional username for authentication.
+        # @param password [String, nil] Optional password for authentication.
+        # @param token [String, nil] Optional token for JWT-based authentication.
+        # @param max_redirects [Integer] Maximum amount of redirects to follow.
+        # @return [String] The downloaded file content.
+        def fetch_file_content(username: nil, password: nil, token: nil, max_redirects: 3)
+          uri = URI.parse(@url)
+          request = Net::HTTP::Get.new(uri)
+
+          request['Authorization'] = "Bearer #{token}" if token
+          request.basic_auth(username, password) if username && password
+
+          response = make_request(uri, request, max_redirects)
+          if response.code.to_i > 299
+            raise "Failed to download file: HTTP status code #{response.code}"
+          elsif response.code.to_i < 200
+            raise "Failed to download file: Invalid response code #{response.code}."
+          end
+
+          response.body
+        end
+
+        private
+
+        def extract_filename_from_url(uri)
+          filename = File.basename(uri.path)
+          filename.empty? ? '' : filename
+        end
+
+        def fill_filename(filename)
+          filename ||= extract_filename_from_url(URI.parse(@url))
+          if filename.empty? || File.extname(filename).empty?
+            filename = generate_file_name(extension: get_file_extension(filename))
+          end
+          filename
+        end
+
+        def make_request(uri, request, max_redirects)
+          Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+            response = http.request(request)
+            if response.is_a?(Net::HTTPRedirection) && max_redirects.positive?
+              location = response['location']
+              raise 'No location in redirection header.' if location.nil?
+
+              new_uri = URI.parse(location)
+              request = Net::HTTP::Get.new(new_uri)
+              make_request(new_uri, request, max_redirects - 1)
+            else
+              response
+            end
+          end
+        end
+
+        def get_file_extension(filename)
+          ext = File.extname(filename)
+          ext.empty? ? nil : ext.downcase
+        end
+
+        def generate_file_name(extension: nil)
+          extension ||= '.tmp'
+          random_string = Array.new(8) { rand(36).to_s(36) }.join
+          "mindee_temp_#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}_#{random_string}#{extension}"
         end
       end
     end
---
+++ spec/input/url_input_source_spec.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require 'mindee'
+require_relative '../http/mock_http_response'
+
+RSpec.describe Mindee::Input::Source::UrlInputSource do
+  let(:valid_url) { 'https://validurl/some/file.jpg' }
+  let(:valid_url_no_filename) { 'https://validurl/some/' }
+  let(:invalid_url) { 'http://invalidurl/some/file.jpg' }
+  let(:output_dir) { "#{DATA_DIR}/output/" }
+
+  describe '#initialize' do
+    context 'with valid URL' do
+      it 'creates a new instance' do
+        input = described_class.new('https://platform.mindee.com')
+        expect(input.url).to eq('https://platform.mindee.com')
+      end
+    end
+
+    context 'with invalid URL' do
+      it 'raises an error for invalid URLs' do
+        expect { described_class.new(invalid_url) }.to raise_error('URL must be HTTPS')
+      end
+    end
+  end
+
+  describe '#as_local_input_source' do
+    let(:url_input_source) { described_class.new(valid_url) }
+    let(:url_input_source_no_filename) { described_class.new(valid_url_no_filename) }
+
+    before do
+      allow(Net::HTTP).to receive(:start).and_return(mock_response)
+    end
+
+    context 'when download is successful' do
+      let(:mock_response) { MockHTTPResponse.new('1.1', '200', 'OK', 'file content') }
+
+      it 'returns a BytesInputSource' do
+        result = url_input_source.as_local_input_source('', filename: 'file.pdf')
+        expect(result).to be_a(Mindee::Input::Source::BytesInputSource)
+        expect(result.filename).to eq('file.pdf')
+        expect(result.io_stream).to be_a(StringIO)
+        expect(result.io_stream.read).to eq('file content')
+      end
+
+      it 'uses a custom filename when provided' do
+        result = url_input_source.as_local_input_source('', filename: 'custom.pdf')
+        expect(result.filename).to eq('custom.pdf')
+      end
+
+      it 'handles authentication' do
+        result = url_input_source.as_local_input_source('', username: 'user', password: 'pass')
+        expect(result).to be_a(Mindee::Input::Source::BytesInputSource)
+      end
+    end
+
+    context 'when download fails' do
+      let(:mock_response) { MockHTTPResponse.new('1.1', '404', 'Not Found', '') }
+
+      it 'raises an error' do
+        expect { url_input_source.as_local_input_source('') }.to raise_error(RuntimeError, %r{Failed to download file})
+      end
+    end
+  end
+
+  describe '#save_to_file' do
+    let(:url_input_source) { described_class.new(valid_url) }
+    let(:url_input_source_no_filename) { described_class.new(valid_url_no_filename) }
+
+    before do
+      allow(Net::HTTP).to receive(:start).and_return(mock_response)
+      allow(File).to receive(:write)
+    end
+
+    context 'when download is successful' do
+      let(:mock_response) { MockHTTPResponse.new('1.1', '200', 'OK', 'file content') }
+
+      it 'generates a valid filename when not provided' do
+        output_file_path = url_input_source_no_filename.save_to_file(output_dir)
+        expect(output_file_path).to match(%r{mindee_temp_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}_[a-z0-9]{8}\.tmp})
+      end
+
+      it 'saves the file with the provided filename' do
+        result = url_input_source.save_to_file('/tmp', filename: 'file.pdf')
+        expect(result).to eq('/tmp/file.pdf')
+        expect(File).to have_received(:write).with('/tmp/file.pdf', 'file content')
+      end
+
+      it 'uses a custom filename when provided' do
+        result = url_input_source.save_to_file('/tmp', filename: 'custom.pdf')
+        expect(result).to eq('/tmp/custom.pdf')
+      end
+
+      it 'handles authentication' do
+        result = url_input_source_no_filename.save_to_file('/tmp', username: 'user', password: 'pass')
+        expect(result).to match(%r{/tmp/mindee_temp_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}_[a-z0-9]{8}\.tmp})
+      end
+    end
+
+    context 'when download fails' do
+      let(:mock_response) { MockHTTPResponse.new('1.1', '404', 'Not Found', '') }
+
+      it 'raises an error' do
+        expect { url_input_source.save_to_file('/tmp') }.to raise_error(RuntimeError, %r{Failed to download file})
+      end
+    end
+  end
+end
