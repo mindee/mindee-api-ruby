98cb4d0bc01ebd3bd18ca935d6cc6810312f9ae7
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2025-03-11 17:10:29 +0100

    :recycle: update CLI syntax for easier product creation

--- bin/mindee.rb
+++ bin/mindee.rb
@@ -5,122 +5,7 @@ require 'bundler/setup'
 require 'optparse'
 require 'mindee'
 
-DOCUMENTS = {
-  "universal" => {
-    description: "Universal document type from API builder",
-    doc_class: Mindee::Product::Universal::Universal,
-    sync: true,
-    async: true,
-  },
-  "cropper" => {
-    description: 'Cropper',
-    doc_class: Mindee::Product::Cropper::CropperV1,
-    sync: true,
-    async: false,
-  },
-  "financial-document" => {
-    description: 'Financial Document',
-    doc_class: Mindee::Product::FinancialDocument::FinancialDocumentV1,
-    sync: true,
-    async: true,
-  },
-  "invoice" => {
-    description: 'Invoice',
-    doc_class: Mindee::Product::Invoice::InvoiceV4,
-    sync: true,
-    async: true,
-  },
-  "international-id" => {
-    description: 'International Id',
-    doc_class: Mindee::Product::InternationalId::InternationalIdV2,
-    sync: false,
-    async: true,
-  },
-  "receipt" => {
-    description: "Expense Receipt",
-    doc_class: Mindee::Product::Receipt::ReceiptV5,
-    sync: true,
-    async: true,
-  },
-  "driver-license" => {
-    description: "Driver License",
-    doc_class: Mindee::Product::DriverLicense::DriverLicenseV1,
-    sync: false,
-    async: true,
-  },
-  "resume" => {
-    description: "Resume",
-    doc_class: Mindee::Product::Resume::ResumeV1,
-    sync: false,
-    async: true,
-  },
-  "passport" => {
-    description: "Passport",
-    doc_class: Mindee::Product::Passport::PassportV1,
-    sync: true,
-    async: false,
-  },
-  "eu-license-plate" => {
-    description: "EU License Plate",
-    doc_class: Mindee::Product::EU::LicensePlate::LicensePlateV1,
-    sync: true,
-    async: false,
-  },
-  "fr-bank-account-details" => {
-    description: "FR Bank Account Details",
-    doc_class: Mindee::Product::FR::BankAccountDetails::BankAccountDetailsV2,
-    sync: true,
-    async: false,
-  },
-  "fr-bank-statement" => {
-    description: "FR Bank Statement",
-    doc_class: Mindee::Product::FR::BankStatement::BankStatementV2,
-    sync: false,
-    async: true,
-  },
-  "fr-id-card" => {
-    description: "FR ID Card",
-    doc_class: Mindee::Product::FR::IdCard::IdCardV2,
-    sync: true,
-    async: false,
-  },
-  "fr-health-card" => {
-    description: "FR Health Card",
-    doc_class: Mindee::Product::FR::HealthCard::HealthCardV1,
-    sync: false,
-    async: true,
-  },
-  "fr-payslip" => {
-    description: "FR Payslip",
-    doc_class: Mindee::Product::FR::Payslip::PayslipV3,
-    sync: false,
-    async: true,
-  },
-  "us-bank-check" => {
-    description: "US Bank Check",
-    doc_class: Mindee::Product::US::BankCheck::BankCheckV1,
-    sync: true,
-    async: false,
-  },
-  "us-heathcare-card" => {
-    description: "US Healthcare Card",
-    doc_class: Mindee::Product::US::HealthcareCard::HealthcareCardV1,
-    sync: false,
-    async: true
-  },
-  "us-w9" => {
-    description: "US W9 Form",
-    doc_class: Mindee::Product::US::W9::W9V1,
-    sync: true,
-    async: false
-  },
-  "invoice-splitter" => {
-    description: "US Bank Check",
-    doc_class: Mindee::Product::InvoiceSplitter::InvoiceSplitterV1,
-    sync: false,
-    async: true,
-  },
-}
+require_relative 'products'
 
 options = {}
 
@@ -136,7 +21,7 @@ def custom_subcommand(cli_parser, options)
 end
 
 product_parser = {}
-DOCUMENTS.each do |doc_key, doc_value|
+PRODUCTS.each do |doc_key, doc_value|
   product_parser[doc_key] = OptionParser.new do |opts|
     opts.on('-w', '--all-words', 'Include words in response') do |v|
       options[:all_words] = v
@@ -170,14 +55,14 @@ end
 global_parser = OptionParser.new do |opts|
   opts.banner = "Usage: mindee.rb product [options] file"
   opts.separator "Available products:"
-  opts.separator "  #{DOCUMENTS.keys.join("\n  ")}"
+  opts.separator "  #{PRODUCTS.keys.join("\n  ")}"
 end
 
 command = ARGV.shift
-unless DOCUMENTS.include?(command)
+unless PRODUCTS.include?(command)
   abort(global_parser.help)
 end
-doc_class = DOCUMENTS[command][:doc_class]
+doc_class = PRODUCTS[command][:doc_class]
 product_parser[command].parse!
 
 if command == 'universal'
@@ -224,7 +109,7 @@ else
 end
 
 if options[:parse_async].nil?
-  if !DOCUMENTS[command][:sync]
+  if !PRODUCTS[command][:sync]
     options[:parse_async] = true
   else
     options[:parse_async] = false
