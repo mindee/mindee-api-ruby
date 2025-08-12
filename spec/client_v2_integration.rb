b4495b49ff1ca18b494d99f52b3ccc4e9fc76464
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-08-12 17:56:55 +0200

    fix integration tests

--- spec/client_v2_integration.rb
+++ spec/client_v2_integration.rb
@@ -17,12 +17,10 @@ RSpec.describe 'Mindee::ClientV2 – integration tests (V2)', :integration, orde
       max_retries: 80
     )
 
-    params = Mindee::Input::InferenceParameters.new(params: {
-                                                      model_id: model_id,
-                                                      rag: false,
-                                                      alias: 'ruby-integration-test',
-                                                      polling_options: polling,
-                                                    })
+    params = Mindee::Input::InferenceParameters.new(model_id,
+                                                    rag: false,
+                                                    file_alias: 'ruby-integration-test',
+                                                    polling_options: polling)
 
     resp = client.enqueue_and_get_inference(input, params)
 
@@ -43,11 +41,9 @@ RSpec.describe 'Mindee::ClientV2 – integration tests (V2)', :integration, orde
     src_path = File.join(__dir__ || './', 'data', 'products', 'financial_document', 'default_sample.jpg')
     input = Mindee::Input::Source::FileInputSource.new(File.open(src_path, 'rb'), 'default_sample.jpg')
 
-    params = Mindee::Input::InferenceParameters.new(params: {
-                                                      model_id: model_id,
-                                                      rag: false,
-                                                      alias: 'ruby-integration-test',
-                                                    })
+    params = Mindee::Input::InferenceParameters.new(model_id,
+                                                    rag: false,
+                                                    file_alias: 'ruby-integration-test')
 
     resp = client.enqueue_and_get_inference(input, params)
 
@@ -65,7 +61,7 @@ RSpec.describe 'Mindee::ClientV2 – integration tests (V2)', :integration, orde
     src_path = File.join(__dir__ || './', 'data', 'file_types', 'pdf', 'blank_1.pdf')
     input = Mindee::Input::Source::FileInputSource.new(File.open(src_path, 'rb'), 'blank_1.pdf')
 
-    params = Mindee::Input::InferenceParameters.new(params: { model_id: 'INVALID_MODEL_ID' })
+    params = Mindee::Input::InferenceParameters.new('INVALID_MODEL_ID')
 
     expect do
       client.enqueue_inference(input, params)
@@ -76,10 +72,8 @@ RSpec.describe 'Mindee::ClientV2 – integration tests (V2)', :integration, orde
     src_path = File.join(__dir__ || './', 'data', 'file_types', 'pdf', 'blank_1.pdf')
     input = Mindee::Input::Source::FileInputSource.new(File.open(src_path, 'rb'), 'blank_1.pdf')
 
-    params = Mindee::Input::InferenceParameters.new(params: {
-                                                      model_id: model_id,
-                                                      webhook_ids: ['INVALID_WEBHOOK_ID'],
-                                                    })
+    params = Mindee::Input::InferenceParameters.new(model_id,
+                                                    webhook_ids: ['INVALID_WEBHOOK_ID'])
 
     expect do
       client.enqueue_inference(input, params)
@@ -95,7 +89,7 @@ RSpec.describe 'Mindee::ClientV2 – integration tests (V2)', :integration, orde
   it 'parses an URL input source without errors' do
     url_input = Mindee::Input::Source::URLInputSource.new(blank_pdf_url)
 
-    params = Mindee::Input::InferenceParameters.new(params: { model_id: model_id })
+    params = Mindee::Input::InferenceParameters.new(model_id)
 
     resp = client.enqueue_and_get_inference(url_input, params)
 
