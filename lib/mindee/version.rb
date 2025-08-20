635508324e5cd7665e302951e31ae5930e80e966
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-08-20 17:05:46 +0200

    :bookmark: Version 4.7.0-rc2 (#198)

--- lib/mindee/version.rb
+++ lib/mindee/version.rb
@@ -3,7 +3,7 @@
 # Mindee
 module Mindee
   # Current version.
-  VERSION = '4.7.0-rc1'
+  VERSION = '4.7.0-rc2'
 
   # Finds and return the current platform.
   # @return [Symbol, Hash[String | Symbol, Regexp], Nil?]
