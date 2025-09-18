e97214a67d4311102867157185811e0b4552f7f0
ianaré <97107275+ianardee@users.noreply.github.com>
2025-09-18 14:48:58 +0200

    :bookmark: Version 4.7.0 (#208)

--- lib/mindee/version.rb
+++ lib/mindee/version.rb
@@ -3,7 +3,7 @@
 # Mindee
 module Mindee
   # Current version.
-  VERSION = '4.7.0-rc3'
+  VERSION = '4.7.0'
 
   # Finds and return the current platform.
   # @return [Symbol, Hash[String | Symbol, Regexp], Nil?]
