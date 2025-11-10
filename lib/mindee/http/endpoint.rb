521b9cdc9864e4425512cd15ea7376551d035d47
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-11-10 17:01:41 +0100

    try another way?

--- lib/mindee/http/endpoint.rb
+++ lib/mindee/http/endpoint.rb
@@ -190,6 +190,7 @@ module Mindee
         req = Net::HTTP::Get.new(uri, headers)
 
         response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, read_timeout: @request_timeout) do |http|
+          configure_ssl(http)
           http.request(req)
         end
 
@@ -213,6 +214,18 @@ module Mindee
               "check your Client Configuration.\nYou can set this using the " \
               "'#{HTTP::API_KEY_ENV_NAME}' environment variable."
       end
+    end
+
+    # Configures SSL settings for HTTP connections
+    # @param http [Net::HTTP]
+    def configure_ssl(http)
+      return unless http.use_ssl?
+
+      # Skip CRL checks in test/CI environments
+      return unless ENV['MINDEE_SKIP_CRL_CHECK']&.downcase == 'true' || ENV['CI']
+
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.ssl_version = :TLSv1_2
     end
   end
 end
