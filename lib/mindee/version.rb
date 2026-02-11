fa6ffe4e34d516f3ae5a6c451cf5dac7c935f91a
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-02-11 11:24:21 +0100

    :bookmark: Version 4.11.0 (#227)

--- lib/mindee/version.rb
+++ lib/mindee/version.rb
@@ -3,7 +3,7 @@
 # Mindee
 module Mindee
   # Current version.
-  VERSION = '4.10.1'
+  VERSION = '4.11.0'
 
   # Finds and return the current platform.
   # @return [Symbol, Hash[String | Symbol, Regexp], Nil?]
