50866e1683170aaaebd46cfe5b3b9a36a5af3674
Ianaré Sévi <ianare@mindee.co>
2026-02-25 16:10:39 +0100

    :bookmark: Version 4.12.0

--- lib/mindee/version.rb
+++ lib/mindee/version.rb
@@ -3,7 +3,7 @@
 # Mindee
 module Mindee
   # Current version.
-  VERSION = '4.11.0'
+  VERSION = '4.12.0'
 
   # Finds and return the current platform.
   # @return [Symbol, Hash[String | Symbol, Regexp], Nil?]
