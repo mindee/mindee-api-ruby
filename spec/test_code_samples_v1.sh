afdcace8dc6d40e7fbb9f8141e0332bf2e00a1d8
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-02-26 12:19:35 +0100

    fix code samples + add env vars

--- spec/test_code_samples_v1.sh
+++ spec/test_code_samples_v1.sh
@@ -2,12 +2,9 @@
 set -e
 
 OUTPUT_FILE='./_test_v1.rb'
-ACCOUNT=$1
-ENDPOINT=$2
-API_KEY=$3
 
-if [ -z "${ACCOUNT}" ]; then echo "ACCOUNT is required"; exit 1; fi
-if [ -z "${ENDPOINT}" ]; then echo "ENDPOINT is required"; exit 1; fi
+if [ -z "${MINDEE_ACCOUNT_SE_TESTS}" ]; then echo "MINDEE_ACCOUNT_SE_TESTS is required"; exit 1; fi
+if [ -z "${MINDEE_ENDPOINT_SE_TESTS}" ]; then echo "MINDEE_ENDPOINT_SE_TESTS is required"; exit 1; fi
 
 for f in $(find ./docs/code_samples -maxdepth 1 -name "*.txt" -not -name "workflow_*.txt" -not -name "v2_*" | sort -h)
 do
@@ -17,18 +14,18 @@ do
   echo "###############################################"
   echo
 
-  sed "s/my-api-key/${API_KEY}/" "$f" > $OUTPUT_FILE
+  sed "s/my-api-key/${MINDEE_API_KEY_SE_TESTS}/" "$f" > $OUTPUT_FILE
   sed -i 's/\/path\/to\/the\/file.ext/.\/spec\/data\/file_types\/pdf\/blank_1.pdf/' $OUTPUT_FILE
 
   if echo "$f" | grep -q "custom_v1.txt"
   then
-    sed -i "s/my-account/$ACCOUNT/g" $OUTPUT_FILE
-    sed -i "s/my-endpoint/$ENDPOINT/g" $OUTPUT_FILE
+    sed -i "s/my-account/$MINDEE_ACCOUNT_SE_TESTS/g" $OUTPUT_FILE
+    sed -i "s/my-endpoint/$MINDEE_ENDPOINT_SE_TESTS/g" $OUTPUT_FILE
   fi
   if echo "${f}" | grep -q "default.txt"
   then
-    sed -i "s/my-account/$ACCOUNT/g" $OUTPUT_FILE
-    sed -i "s/my-endpoint/$ENDPOINT/g" $OUTPUT_FILE
+    sed -i "s/my-account/$MINDEE_ACCOUNT_SE_TESTS/g" $OUTPUT_FILE
+    sed -i "s/my-endpoint/$MINDEE_ENDPOINT_SE_TESTS/g" $OUTPUT_FILE
     sed -i "s/my-version/1/g" $OUTPUT_FILE
   fi
 
--- spec/test_code_samples_v2.sh
+++ spec/test_code_samples_v2.sh
@@ -2,16 +2,6 @@
 set -e
 
 OUTPUT_FILE='./_test_v2.rb'
-API_KEY_V2=$1
-FINDOC_MODEL_ID=$2
-WEBHOOK_ID=$3
-CLASSIFICATION_MODEL_ID=$4
-CROP_MODEL_ID=$5
-OCR_MODEL_ID=$6
-SPLIT_MODEL_ID=$7
-
-if [ -z "${FINDOC_MODEL_ID}" ]; then echo "FINDOC_MODEL_ID is required"; exit 1; fi
-if [ -z "${WEBHOOK_ID}" ]; then echo "WEBHOOK_ID is required"; exit 1; fi
 
 for f in $(find ./docs/code_samples -maxdepth 1 -name "v2_*.txt" | sort -h)
 do
@@ -22,38 +12,38 @@ do
   echo
 
   cat "${f}" > $OUTPUT_FILE
-  sed -i "s/MY_API_KEY/$API_KEY_V2/" $OUTPUT_FILE
-  sed -i "s/MY_WEBHOOK_ID/$WEBHOOK_ID/" $OUTPUT_FILE
+  sed -i "s/MY_API_KEY/${MINDEE_V2_API_KEY}/" $OUTPUT_FILE
+  sed -i "s/MY_WEBHOOK_ID/${MINDEE_V2_SE_TESTS_FAILURE_WEBHOOK_ID}/" $OUTPUT_FILE
   sed -i 's/\/path\/to\/the\/file.ext/.\/spec\/data\/file_types\/pdf\/blank_1.pdf/' $OUTPUT_FILE
 
   if echo "${f}" | grep -q "v2_default.txt"
   then
-    sed -i "s/MY_MODEL_ID/$FINDOC_MODEL_ID/" $OUTPUT_FILE
+    sed -i "s/MY_MODEL_ID/${MINDEE_V2_SE_TESTS_FINDOC_MODEL_ID}/" $OUTPUT_FILE
   fi
 
   if echo "${f}" | grep -q "v2_default_webhook.txt"
   then
-    sed -i "s/MY_MODEL_ID/$FINDOC_MODEL_ID/" $OUTPUT_FILE
+    sed -i "s/MY_MODEL_ID/${MINDEE_V2_SE_TESTS_FINDOC_MODEL_ID}/" $OUTPUT_FILE
   fi
 
   if echo "${f}" | grep -q "v2_classification.txt"
   then
-    sed -i "s/MY_MODEL_ID/$CLASSIFICATION_MODEL_ID/" $OUTPUT_FILE
+    sed -i "s/MY_MODEL_ID/${MINDEE_V2_SE_TESTS_CLASSIFICATION_MODEL_ID}/" $OUTPUT_FILE
   fi
 
   if echo "${f}" | grep -q "v2_crop.txt"
   then
-    sed -i "s/MY_MODEL_ID/$CROP_MODEL_ID/" $OUTPUT_FILE
+    sed -i "s/MY_MODEL_ID/${MINDEE_V2_SE_TESTS_CROP_MODEL_ID}/" $OUTPUT_FILE
   fi
 
   if echo "${f}" | grep -q "v2_ocr.txt"
   then
-    sed -i "s/MY_MODEL_ID/$OCR_MODEL_ID/" $OUTPUT_FILE
+    sed -i "s/MY_MODEL_ID/${MINDEE_V2_SE_TESTS_OCR_MODEL_ID}/" $OUTPUT_FILE
   fi
 
   if echo "${f}" | grep -q "v2_split.txt"
   then
-    sed -i "s/MY_MODEL_ID/$SPLIT_MODEL_ID/" $OUTPUT_FILE
+    sed -i "s/MY_MODEL_ID/${MINDEE_V2_SE_TESTS_SPLIT_MODEL_ID}/" $OUTPUT_FILE
   fi
 
   bundle exec ruby $OUTPUT_FILE
