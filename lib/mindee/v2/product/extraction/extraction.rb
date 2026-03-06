ea586f236b7e6b714d76ca958fb8eea5cda6dca1
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-03-06 10:11:36 +0100

    temp

--- lib/mindee/v2/product/extraction/extraction.rb
+++ lib/mindee/v2/product/extraction/extraction.rb
@@ -8,10 +8,35 @@ module Mindee
     module Product
       module Extraction
         # Extraction product.
-        # Note: currently a placeholder for the `Inference` class.
-        class Extraction < Mindee::Parsing::V2::Inference
+        class Extraction < Mindee::V2::Parsing::BaseInference
           @params_type = Product::Extraction::Params::ExtractionParameters
           @response_type = Product::Extraction::ExtractionResponse
+          # @return [InferenceActiveOptions] Options which were activated during the inference.
+          attr_reader :active_options
+          # @return [InferenceResult] Result contents.
+          attr_reader :result
+
+          @params_type = Input::InferenceParameters
+          @slug = 'extraction'
+          @response_type = InferenceResponse
+
+          # @param server_response [Hash] Hash representation of the JSON returned by the service.
+          def initialize(server_response)
+            super
+            @active_options = InferenceActiveOptions.new(server_response['active_options'])
+            @result = InferenceResult.new(server_response['result'])
+          end
+
+          # String representation.
+          # @return [String]
+          def to_s
+            [
+              super,
+              @active_options.to_s,
+              @result.to_s,
+              '',
+            ].join("\n")
+          end
         end
       end
     end
