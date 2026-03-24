4c8c5b105161feba8e95aee44e142738dc9bb333
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-03-24 17:02:00 +0100

    :recycle: :boom: make logging configurable and default output to stderr

--- lib/mindee/logging/logger.rb
+++ lib/mindee/logging/logger.rb
@@ -5,8 +5,16 @@ require 'logger'
 module Mindee
   # Mindee logging module.
   module Logging
-    @logger = Logger.new($stdout)
     log_level = ENV.fetch('MINDEE_LOG_LEVEL', 'WARN')
+    log_output = ENV.fetch('MINDEE_LOG_OUTPUT', 'stderr')
+    @logger = if log_output == 'stderr'
+                Logger.new($stderr)
+              elsif log_output == 'stdout'
+                Logger.new($stdout)
+              else
+                warn "Invalid MINDEE_LOG_OUTPUT='#{log_output}', defaulting to 'stderr'"
+                Logger.new($stderr)
+              end
     @logger.level = Logger.const_get(log_level)
 
     class << self
