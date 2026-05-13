a67f40aacbb1a4b6b43a796cc15220082092f112
ianaré <97107275+ianardee@users.noreply.github.com>
2026-05-13 20:48:22 +0200

    :bookmark: Version 5.1.0 (#248)

--- lib/mindee/version.rb
+++ lib/mindee/version.rb
@@ -3,7 +3,7 @@
 # Mindee
 module Mindee
   # Current version.
-  VERSION = '5.0.0'
+  VERSION = '5.1.0'
 
   # Finds and return the current platform.
   # @return [Symbol, Hash[String | Symbol, Regexp], Nil?]
