d9129030df768b3ea38dd1861a52b2dc5c57268c
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-06-18 10:10:06 +0200

    :recycle: add secondary accessor for search model pagination to match json return (#254)

--- lib/mindee/v2/parsing/search/search_response.rb
+++ lib/mindee/v2/parsing/search/search_response.rb
@@ -6,16 +6,19 @@ module Mindee
       module Search
         # Models search response.
         class SearchResponse < CommonResponse
-          # @return [Search::Search] Parsed search payload.
+          # @return [Search::SearchModels] Parsed search payload.
           attr_reader :models
-          # @return [Search::Search] Pagination metadata.
+          # @return [Search::PaginationMetadata] Pagination metadata.
+          attr_reader :pagination
+          # @return [Search::PaginationMetadata] Pagination metadata.
           attr_reader :pagination_metadata
 
           def initialize(server_response)
             super
 
-            @models = Search::SearchModels.new(server_response['models'])
-            @pagination_metadata = PaginationMetadata.new(server_response['pagination'])
+            @models = SearchModels.new(server_response['models'])
+            @pagination = PaginationMetadata.new(server_response['pagination'])
+            @pagination_metadata = @pagination
           end
 
           # String representation.
@@ -27,7 +30,7 @@ module Mindee
               @models.to_s,
               'Pagination Metadata',
               '###################',
-              @pagination_metadata.to_s,
+              @pagination.to_s,
               '',
             ].join("\n")
           end
